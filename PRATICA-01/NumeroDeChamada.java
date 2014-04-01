3c230fd8e3e76c1221fd062476415fd039829ae0
Luis Farias <fernandokf@terra.com.br>
2014-04-01 14:26:52 -0300

    Atualização do código

--- PRATICA-01/NumeroDeChamada.java
+++ PRATICA-01/NumeroDeChamada.java
@@ -7,7 +7,14 @@ termos (por exemplo, repetir a palavra autor, ou título). Para a busca se torna
 Ainda não pensei quais métodos vou implementar.
 
 - Farias(usuário github/farias01)
+*/  
+
+/* A descrição do ISBN-13 pode ser encontrada na wikipedia. Não sei exatamente como se implementa o número de chamada. Pelo que pesquisei brevemente o sistema mais antigo e que ainda é utilizado em pequena escala é a Classificação Decimal Dewey (ou DDC), que tem a desvantagem de ser um sistema proprietário. Uma boa alternativa é o sistema de classificação "Library of Congress Classification (LCC) http://authorities.loc.gov/ " que é mais moderno e livre.
+Um possível método para esta classe é para verificar o checksum do ISBN-13.
+
+- Farias(usuário github/farias01)
 */
+
 
 /* Cada vez que editar este arquivo (a parte do código) coloque a tua idéia acima desta linha e "assine". Não altere as postagens anteriores
 da "Metainformação de desenvolvimento" inclusive as tuas próprias. - Separe cada postagem usando /*postagem* / ao invés de //   */
@@ -18,10 +25,10 @@ da "Metainformação de desenvolvimento" inclusive as tuas próprias. - Separe c
 
 // Para compatibilidade com sistemas de arquivos que não suportam utf-8 mantemos os nomes das classes restritos aos caracteres ASCII
 
-class FichaCatalografica {
-	private Descricao descricao;
-	private PontoDeAcesso ponto_de_acesso;
-	private NumeroDeChamada numero_de_chamada;
+class NumeroDeChamada {
+	private double ISBN_13 ;
+	private String LCC;
+
 }	
 
 
